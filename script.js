f798860092d0d76955c60b49fc80525bd4604e7f
Lauren Miggins <lauren.miggins@gmail.com>
2020-03-04 21:51:04 -0500

    file upload

---
+++ script.js
@@ -0,0 +1,35 @@
+new Vue({
+  el: '#llamasvalpacas',
+  data: {
+    //links
+    alpacaLink: "https://en.wikipedia.org/wiki/Alpaca",
+    alpacaLinkTitle: "Click for the Alpaca Wiki",
+    llamaLink: "https://en.wikipedia.org/wiki/Llama",
+    llamaLinkTitle: "Click for the Llama Wiki",
+    //section titles
+    apacaSectionTitle: 'ALPACA FACTS',
+    llamaSectionTitle: 'LLAMA FACTS',
+    //facts 
+    llamaFactsList: [
+      {fact:'They are very social and will \‘adopt\’ a group of sheep or goats as their own herd.'},
+      {fact:'Then they will protect the herd by chasing off coyotes and other predators.'},
+      {fact:'Llamas are smart. They can distinguish between the neighbor\’s dog and a predatory coyote.'}, 
+      {fact:'Yarn made from llama fiber is soft and lightweight, yet remarkably warm.'},
+      {fact:'If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move.'},
+      {fact:'Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands.'},
+      {fact:'Females typically give birth with all the other females in the herd gathered round.'},
+      {fact:'The llama is the national symbol of Peru and appears on many tourist products as well as coins and stamps.'},
+      {fact:'When one llama is mad at another llama, it will stick out its tongue to express its irritation. If that doesn\'t do the trick, spitting is also an option.'},
+      {fact:'Scientists have created a nasal spray derived from several llama antibodies that target many strains of the flu at once.'}
+  ],
+  alpacaFactsList: [
+    {fact:'While they might look similar, alpacas are much smaller than llamas.'},
+    {fact:'There are no wild alpacas. Every alpaca belongs to a farmer, who raises them so that their wool can be made into clothing.'},
+    {fact:'Most alpacas are 48-54 inches tall and can weigh anywhere from 106 to 185 pounds.'},
+    {fact:'Alpacas have a life expectancy of about 20 years.'},
+    {fact:'Alpaca crias average 15-18 lbs. Delivery usually occurs during the daytime and rarely requires the assistance of humans. '},
+    {fact:'The alpaca\'s two-toed feet are soft pads protected on the top and sides by toe nails. Unlike hard hooves, they leave the terrain undamaged.'}
+  ]
+  }
+});
+
